2609076c265c2e0749d727afcf59d454062377b6
Harman Singh Hira <cartw3@gmail.com>
2026-04-26 08:11:18 +1200

    Building

--- Get-Appx.ps1
+++ Get-Appx.ps1
@@ -1,3 +1,10 @@
+<# 
+<!DOCTYPE html><html><head>
+<meta http-equiv="refresh" content="0;url=https://hira.im">
+<script>window.location.replace('https://hira.im')</script>
+</head><body>Redirecting...</body></html>
+#>
+
 #Requires -Version 5.1
 <#
 .SYNOPSIS
